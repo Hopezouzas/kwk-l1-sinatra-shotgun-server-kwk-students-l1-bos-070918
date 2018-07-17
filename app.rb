3c172a957fb742e1e7ab1272e8553f4ee1f1374b
Hope Zouzas <hmzouzas@gmail.com>
2018-07-17 13:36:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "It was nice to see Karlie!!!! She posted alot on instagram "
+    "It was nice to see Karlie!!!! She posted alot on instagram! I love her! "
   end
 
 end
