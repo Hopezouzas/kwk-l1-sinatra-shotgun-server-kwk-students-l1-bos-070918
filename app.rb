e2d44323ab22e836ef87ada4a08cb15d7a0dbd04
Hope Zouzas <hmzouzas@gmail.com>
2018-07-17 13:43:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,10 @@ class App < Sinatra::Base
   get '/' do 
     "It was nice to see Karlie!!!! She posted alot on instagram! I love her! "
   end
-
+get '/kitty' do 
+  "Hello Kitty!"
+end
+get '/lollipops' do 
+  "lollipops are good"
+end 
 end
