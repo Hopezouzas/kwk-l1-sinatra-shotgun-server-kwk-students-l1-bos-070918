e0ce8f1733bf0cbe59c614e1216f0c3a2339d2cc
Hope Zouzas <hmzouzas@gmail.com>
2018-11-30 14:04:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "It was nice to see Karlie!!!! She posted alot on instagram! I love her! "
   end
-
+  get '/kitty' do 
+  "Hello Kitty!"
+end
+  get '/lollipops' do 
+  "lollipops are good"
+end 
 end
